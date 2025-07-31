2ba804c70db67f787670cbb2298f84aceefe9360
Nguyen-Thanh-Tung <nguyenthanhtung8196@gmail.com>
2025-07-31 18:32:34 +0900

    add library and dma project

---
+++ normal/make_dev/wp1/02-DMA-mem/dma.h
@@ -0,0 +1,27 @@
+/******************************************************************************/
+/*! @addtogroup Group2
+    @file       dma.h
+    @brief      
+    @date       2025/07/31
+    @author     Development Dept at Tokyo (nguyen-thanh-tung@jcm-hq.co.jp)
+    @par        Revision
+    $Id$
+    @par        Copyright (C)
+    Japan CashMachine Co, Limited. All rights reserved.
+******************************************************************************/
+#ifndef _DMA_H_
+#define _DMA_H_
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* CODE */
+#include "../../sdk/Device/Include/stm32f746xx.h"
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
---
+++ normal/make_dev/wp1/02-DMA-mem/main.c
@@ -0,0 +1,27 @@
+/******************************************************************************/
+/*! @addtogroup Group2
+    @file       main.c
+    @brief      
+    @date       2025/07/30
+    @author     Development Dept at Tokyo (nguyen-thanh-tung@jcm-hq.co.jp)
+    @par        Revision
+    $Id$
+    @par        Copyright (C)
+    Japan CashMachine Co, Limited. All rights reserved.
+******************************************************************************/
+
+#include <stdint.h>
+#include <stdio.h>
+
+
+int main()
+{
+    printf("DMA memmory to memory\n");
+    while (1)
+    {
+        /* code */
+
+    }
+}
+
+
